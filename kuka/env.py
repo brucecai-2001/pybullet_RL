0d5623e951f400b2d4ef33e549e33f1b7d56b1ab
蔡欣宇 <cxy1454272125@126.com>
2024-07-18 23:07:47 +0800

    kuka stable baselines

--- kuka/env.py
+++ kuka/env.py
@@ -256,8 +256,7 @@ class KukaReachVisualEnv(gym.Env):
 
         if image is not None:
             image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-            image = cv2.resize(image, (self.kImageSize['width'], self.kImageSize['height']))[None, :, :]
-            # image = cv2.resize(image, (self.kImageSize['width'], self.kImageSize['height']))[None, :, :] / 255.
+            image = cv2.resize(image, (self.kImageSize['width'], self.kImageSize['height']))[None, :, :] / 255
             return image
         else:
             return np.zeros((1, self.kImageSize['width'], self.kImageSize['height']))
