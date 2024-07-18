bcc921743e97b3cf8a21215ad17bb809f2798154
蔡欣宇 <cxy1454272125@126.com>
2024-07-19 02:03:55 +0800

    kuka stable baselines

--- kuka/train_ppo.py
+++ kuka/train_ppo.py
@@ -1,11 +1,13 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.env_util import make_vec_env
 from env import KukaReachVisualEnv
 
 kuka_env = KukaReachVisualEnv()
-check_env(kuka_env, warn=True)
+env = make_vec_env(kuka_env, n_envs=1)
+#check_env(kuka_env, warn=True)
 
-model = PPO("CnnPolicy", kuka_env, verbose=1)
+model = PPO("CnnPolicy", env, verbose=1)
 model.learn(total_timesteps=25000)
 model.save("kuka_ppo")
 
@@ -16,5 +18,5 @@ model = PPO.load("kuka_ppo")
 obs, _ = kuka_env.reset()
 while True:
     action, _ = model.predict(obs)
-    obs, rewards, dones, _,  info = kuka_env.step(action)
+    obs, rewards, dones, truncated,  info = kuka_env.step(action)
     kuka_env.render("human")
