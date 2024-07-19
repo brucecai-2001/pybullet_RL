7e0362f25a57b047e575ef16f31c5c49a85cf29d
蔡欣宇 <cxy1454272125@126.com>
2024-07-19 19:52:16 +0800

    panda

--- kuka/train_ppo.py
+++ kuka/train_ppo.py
@@ -2,11 +2,15 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from env import KukaReachVisualEnv
 
-env = make_vec_env(KukaReachVisualEnv, n_envs=1)
+env = make_vec_env(KukaReachVisualEnv, 
+                   n_envs=1)
 
-model = PPO("CnnPolicy", env, verbose=1)
+model = PPO("CnnPolicy", 
+            env, 
+            verbose=1
+        )
+
 model.learn(total_timesteps=25000, progress_bar=True)
-model.save("kuka_ppo")
 
-del model # remove to demonstrate saving and loading
+model.save("kuka_ppo")
 
--- panda/train_ppo.py
+++ panda/train_ppo.py
@@ -1,8 +1,25 @@
-import gym
+import gymnasium as gym
+import panda_gym
 from stable_baselines3 import PPO
 
-env = gym.make("PandaReach-v2")
+env = gym.make(
+    "PandaReach-v3",
+    render_mode="human",
+    reward_type="dense",  # "dense" or "sparse"
+    control_type="ee",  # "ee" or "joints"
+)
 
-model = PPO("MultiInputPolicy", env, verbose=1)
-model.learn(total_timesteps=25000, progress_bar=True)
-model.save("panda_ppo")
+# Set up PPO model
+model = PPO(
+    "MultiInputPolicy",
+    env,
+    verbose=1,
+    tensorboard_log="runs",
+    learning_rate=0.001,
+)
+
+# Train agent
+model.learn(total_timesteps=200000, progress_bar=True)
+
+# Save trained model
+model.save("PandaReach_PPO")
