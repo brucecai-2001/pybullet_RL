ac56a19d3bbb6c067c77662f90040c407e80bdb1
蔡欣宇 <cxy1454272125@126.com>
2024-07-21 15:48:42 +0800

    add panda grasp

---
+++ panda_grasp/panda_grasp.py
@@ -0,0 +1,37 @@
+import pybullet as p
+import pybullet_data as pd
+import math
+import time
+import numpy as np
+import panda_grasp.env as panda_sim
+
+
+fps=240.
+timeStep = 1./fps
+
+def run():
+	p.connect(p.GUI)
+
+	# 设置仿真环境
+	p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP, 1)	# 使Y轴向上, 默认Z轴向上
+	# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+	p.setPhysicsEngineParameter(maxNumCmdPer1ms=1000)
+	p.resetDebugVisualizerCamera(cameraDistance=1.3, cameraYaw=38, cameraPitch=-22, cameraTargetPosition=[0.35,-0.13,0])
+	p.setAdditionalSearchPath(pd.getDataPath())
+	p.setTimeStep(timeStep)
+	p.setGravity(0,-9.8,0)
+
+	# 初始化panda环境
+	env = panda_sim.PandaEnv(p, [0,0,0], fps)
+
+	graspWidthId = p.addUserDebugParameter("graspWidth", 0, 0.06, 0.01)
+
+	for i in range (100000):
+		graspWidth = float(p.readUserDebugParameter(graspWidthId))
+		env.step(graspWidth)
+		p.stepSimulation()
+		time.sleep(timeStep)
+
+
+if __name__ == "__main__":
+	run()
